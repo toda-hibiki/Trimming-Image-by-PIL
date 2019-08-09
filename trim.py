c4d7f6498127c0fac683184104f97f8d79c686c7
toda-hibiki <43288669+toda-hibiki@users.noreply.github.com>
2019-08-09 15:03:12 +0900

    Update trim.py

--- trim.py
+++ trim.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-im = Image.open('vessel1-1.eps')
+im = Image.open('Image1.eps')
 
 #im_crop = im.crop((0,1024,512,1536))
 im_crop = im.crop((0,512,0,512))
-im_crop.save('670_suda.png', quality=95)
+im_crop.save('Image2.png', quality=95)
